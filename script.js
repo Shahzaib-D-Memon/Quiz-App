42cd050e9b41e2e7de01a274e62c407f153963fd
Shahzaib Memon <57555093+Shahzaib-D-Memon@users.noreply.github.com>
2024-08-13 15:14:21 +0500

    Update script.js

--- script.js
+++ script.js
@@ -82,7 +82,6 @@ const optionList = document.querySelector(".option-list");
 // function showQuestions(index) {
 //   const questionText = document.querySelector(".question-text");
 //   questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
-
 //   let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>
 //   <div class="option"><span>${questions[index].options[1]}</span></div>
 //   <div class="option"><span>${questions[index].options[2]}</span></div>
